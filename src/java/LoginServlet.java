9520a7767f34d87586849bb02d826409feb88295
pragati-9036 <pgujar23cs@student.mes.ac.in>
2026-01-11 16:07:08 +0530

    Update LoginServlet.java

--- src/java/LoginServlet.java
+++ src/java/LoginServlet.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
