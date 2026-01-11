4d9f32ccf1d9fa3cb501372ace0e3dd3f37757c1
pragati-9036 <pgujar23cs@student.mes.ac.in>
2026-01-11 16:09:10 +0530

    Update HomePageServlet.java

--- src/java/HomePageServlet.java
+++ src/java/HomePageServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/HomePageServlet")
+
 public class HomePageServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
