ec216fed1c33f8aeb3cca90180b2e247e84ddcda
pragati-9036 <pgujar23cs@student.mes.ac.in>
2026-01-11 16:08:39 +0530

    Update DeleteOrderServlet.java

--- src/java/DeleteOrderServlet.java
+++ src/java/DeleteOrderServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/DeleteOrderServlet")
+
 public class DeleteOrderServlet extends HttpServlet {
      protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
